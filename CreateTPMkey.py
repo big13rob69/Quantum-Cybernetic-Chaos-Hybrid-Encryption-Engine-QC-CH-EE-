4e39665bc6099fa5890cb23887b65848927da621
Robert Robinson <Robert13robinson102009@gmail.com>
2025-09-06 01:14:52 -0400

    Update CreateTPMkey.py
    Signed-off-by: Robert Robinson <Robert13robinson102009@gmail.com>

--- CreateTPMkey.py
+++ CreateTPMkey.py
@@ -1,12 +1,10 @@
-from tpm2_pytss import FAPI
-
 fapi = FAPI()
 key_created = fapi.create_key(
-    path=b"/HS/SRK/new_signing_key",  # Path to store the key
-    type_="sign",  # Key type (e.g., for signing; other options: decrypt, restricted)
-    policy_path=b"/policy/my_policy",  # Optional policy for access control
-    auth_value=b"my_password",  # Authentication password
-    exists_ok=True  # Don't error if path exists
+    path=b"/HS/SRK/new_signing_key",
+    type_="sign",
+    policy_path=b"/policy/my_policy",
+    auth_value=b"my_password",
+    exists_ok=True
 )
-print(f"Key created: {key_created}")
+print("Key created: {}".format(key_created))
 fapi.close()  # Clean up
